49917e487793879dd863f3d0b2becaa5102a6a76
indranil1741997 <indranil1741997@gmail.com>
2020-05-17 01:38:22 +0530

    Added Pets and Contact info to owners on startup, closes #26

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,30 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Indranil");
         owner1.setLastName("Sarkar");
+        owner1.setAddress("123 India");
+        owner1.setCity("Bangalore");
+        owner1.setPhone("789324234");
+
+        Pet indranilPet = new Pet();
+        indranilPet.setPetType(savedDogPetType);
+        indranilPet.setOwner(owner1);
+        indranilPet.setBirthDate(LocalDate.now());
+        indranilPet.setName("Rosco");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Kakoli");
         owner2.setLastName("Sarkar");
+        owner2.setAddress("123 India");
+        owner2.setCity("Bangalore");
+        owner2.setPhone("8904129612");
+
+        Pet kakoliPet = new Pet();
+        kakoliPet.setPetType(savedCatPetType);
+        kakoliPet.setOwner(owner2);
+        kakoliPet.setBirthDate(LocalDate.now());
+        kakoliPet.setName("Porter");
 
         ownerService.save(owner2);
         System.out.println("Loaded Owners........");
