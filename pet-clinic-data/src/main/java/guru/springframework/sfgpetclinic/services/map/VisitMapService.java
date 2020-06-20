ece03ae7e46ba2afecd54cd27af37a8408286643
indranil1741997 <indranil1741997@gmail.com>
2020-06-20 22:02:03 +0530

    Implemented JPA Visit Service and added loader data, closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -9,12 +9,6 @@ import java.util.Set;
 @Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
-    private final VisitService visitService;
-
-    public VisitMapService(VisitService visitService) {
-        this.visitService = visitService;
-    }
-
     @Override
     public Set<Visit> findAll() {
         return super.findAll();
